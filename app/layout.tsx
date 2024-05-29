8ebc02e558997bc14ba4790b4f5a2d3c708d21c1
nitinroyal12 <nitinthakur12390@gmail.com>
2024-05-29 12:56:17 +0530

    some chnages

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
